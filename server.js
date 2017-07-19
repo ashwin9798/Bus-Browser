ffd6796b6c54bd0ff6c6966890ad69f54d7d0312
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-19 20:23:43 +0800

    deploy to heroku, create mySQL connection

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var app = express();
+
+var connection = mysql.createConnection({
+    host: 'us-cdbr-iron-east-03.cleardb.net'
+    username: 'b53283652bc954'
+    password: '86127b66'
+    database: 'heroku_68e1a2399363654'
+})
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -14,6 +22,10 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.json({ message: 'Welcome to map app!' });
 });
+
+router.post('/', function(req, res) {
+
+})
 
 app.use(router);
 app.listen(port);
