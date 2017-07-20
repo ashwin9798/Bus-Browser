26866e3bab50708626f317aaff26452fea6e97c4
Ashwin Vivek <ashwin9798@gmail.com>
2017-07-20 18:34:52 +0800

    add cors for cross domain tests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var cors = require('cors');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json())
+app.use(cors())
 
 var port = process.env.PORT || 3000;
 
