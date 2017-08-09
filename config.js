0bd18567d8e6228e2970b211a6c82f93502fb795
Ashwin Vivek <ashwin9798@gmail.com>
2017-08-09 17:45:22 +0800

    whoops

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.host = 'us-cdbr-iron-east-03.cleardb.net';
-config.user = 'b53283652bc954';
-config.password = '86127b66';
-config.database = 'heroku_68e1a2399363654';
-
-module.exports = config
